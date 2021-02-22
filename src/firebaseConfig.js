c989ae76f6782a9cac937e4cf053bb127c294b9c
ruslannovik <novik.rv93@gmail.com>
2021-02-22 21:35:27 +0300

    feat: add firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyClViBygyvRmhfWcInnnP6yXAKn6JFuJGo',
+  authDomain: 'clever-to-do-list-29817.firebaseapp.com',
+  projectId: 'clever-to-do-list-29817',
+  storageBucket: 'clever-to-do-list-29817.appspot.com',
+  messagingSenderId: '910035101196',
+  appId: '1:910035101196:web:c4e0a2b9c9dbdb48e1b029',
+};
+
+firebase.initializeApp(firebaseConfig);
