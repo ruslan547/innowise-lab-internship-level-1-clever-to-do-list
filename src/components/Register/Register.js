1b94b43f4086433fc8defdae3437e62d459522d8
ruslannovik <novik.rv93@gmail.com>
2021-02-23 17:12:07 +0300

    feat: add Register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import Button from '../UI/Button/Button';
+import Form from '../UI/Form/Form';
+import './Register.scss';
+import arrow from '../../img/arrow.svg';
+
 function Register() {
-  return 'Register';
+  return (
+    <div className="register">
+      <Link className="register__link" to="/signin">
+        <img src={arrow} alt="" />
+        Sign in
+      </Link>
+      <Form onSubmit={console.log('on')}>
+        <Button value="Register" />
+      </Form>
+    </div>
+  );
 }
 
 export default Register;
