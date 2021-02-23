20a21d4b28742eb1a5e15af8ddc7d324a1bd1266
ruslannovik <novik.rv93@gmail.com>
2021-02-24 01:53:27 +0300

    feat: add PrivateRouter

---
+++ src/components/PrivateRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function PrivateRoute({ component: Component }) {
+  const { currentUser } = useAuth();
+
+  return (
+    <Route
+      render={() => {
+        return currentUser ? <Component /> : <Redirect to="/signin" />;
+      }}
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+};
