8a6eee62cad7318759eaed38abd57fa57849cca2
ruslannovik <novik.rv93@gmail.com>
2021-02-25 10:25:46 +0300

    init: TaskPage

--- src/components/TaskPage/TaskPage.js
+++ src/components/TaskPage/TaskPage.js
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
 import './TaskPage.scss';
 
 function TaskPage() {
-  return 'Task';
+  return (
+    <div className="task-page">
+      <div className="task-page__nav">
+        <Link className="signin__link" to="/register">
+          Today{"' "}s Task
+          <div className="signin__arrow" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default TaskPage;
