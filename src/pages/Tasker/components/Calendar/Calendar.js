b1a4209de8534752ea9375eed2c2ab6ff8681ee3
ruslannovik <novik.rv93@gmail.com>
2021-03-07 11:42:28 +0300

    feat: add useMemo to CalendarCard

--- src/pages/Tasker/components/Calendar/Calendar.js
+++ src/pages/Tasker/components/Calendar/Calendar.js
@@ -1,22 +1,20 @@
 /* eslint-disable prettier/prettier */
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import './Calendar.scss';
 import {
   startOfDay,
-  isToday,
-  isEqual,
   addMonths,
   eachDayOfInterval,
-  addDays,
+  // addDays,
   getTime,
 } from 'date-fns';
 import CalendarCard from '../CalendarCard/CalendarCard';
 import enableScroll from './enableScroll';
-import { useApp } from '../../../../core/components/AppProvider/AppProvider';
 
 const INCREMENT = 1;
 
 const createDates = (date) => {
+  console.log('init date');
   return eachDayOfInterval({
     start: date,
     end: addMonths(date, INCREMENT),
@@ -24,62 +22,38 @@ const createDates = (date) => {
 };
 
 function Calendar() {
-  const { currentDate, setCurrentDate } = useApp();
-  const initDate = startOfDay(new Date());
-  const [dates, setDates] = useState(
-    useMemo(() => createDates(initDate)),
-    [],
-  );
+  // const [dates] = useState(() => createDates(startOfDay(new Date())));
+  const dates = useMemo(() => createDates(startOfDay(new Date())), []);
   console.log('calendar');
 
-  const handleClick = useCallback((date) => {
-    setCurrentDate(getTime(date));
-  }, []);
-
-  const createDayClass = useCallback(
-    (date) => {
-      let dayClass = 'card__day';
-
-      if (isToday(date)) {
-        dayClass += ' card__day_today';
-      }
-
-      if (isEqual(currentDate, date)) {
-        dayClass += ' card__day_current';
-      }
-      return dayClass;
-    },
-    [currentDate],
-  );
-
-  const cards = useMemo(
-    () => dates.map((item) => {
+  const createCards = () => {
+    console.log('createCards');
+    return dates.map((item) => {
       return (
         <CalendarCard
           key={item}
-          date={item}
-          onClick={handleClick}
-          createDayClass={createDayClass}
+          date={getTime(item)}
         />
       );
-    }), [currentDate]);
-
-  const addDates = (data) => {
-    return new Promise((resolve) => {
-      const target = document.querySelector('.calendar');
-      const maxScrollLeft = target.scrollWidth - target.clientWidth;
-      if (target.scrollLeft === maxScrollLeft) {
-        const lastDate = data[data.length - 1];
-        const newDates = createDates(addDays(new Date(lastDate), INCREMENT));
-        resolve(newDates);
-      }
     });
   };
+
+  // const addDates = (data) => {
+  //   return new Promise((resolve) => {
+  //     const target = document.querySelector('.calendar');
+  //     const maxScrollLeft = target.scrollWidth - target.clientWidth;
+  //     if (target.scrollLeft === maxScrollLeft) {
+  //       const lastDate = data[data.length - 1];
+  //       const newDates = createDates(addDays(new Date(lastDate), INCREMENT));
+  //       resolve(newDates);
+  //     }
+  //   });
+  // };
 
   const handleWheel = (event) => {
     const target = document.querySelector('.calendar');
     enableScroll(event, target);
-    addDates(dates).then((response) => setDates([...dates, ...response]));
+    // addDates(dates).then((response) => setDates([...dates, ...response]));
   };
 
   useEffect(() => {
@@ -87,7 +61,9 @@ function Calendar() {
     target.addEventListener('wheel', handleWheel);
   }, []);
 
+  const cards = useMemo(() => createCards(), []);
+
   return <ul className="calendar">{cards}</ul>;
 }
 
-export default Calendar;
+export default React.memo(Calendar);
--- src/pages/Tasker/components/CalendarCard/CalendarCard.js
+++ src/pages/Tasker/components/CalendarCard/CalendarCard.js
@@ -1,42 +1,56 @@
 import './CalendarCard.scss';
 import PropTypes from 'prop-types';
-import { format } from 'date-fns';
+import { format, getDate } from 'date-fns';
 import isEqual from 'date-fns/isEqual';
 import React, { useMemo } from 'react';
+import isToday from 'date-fns/isToday';
 import { useApp } from '../../../../core/components/AppProvider/AppProvider';
 
-function CalendarCard({ date, onClick, createDayClass }) {
-  const { tasks } = useApp();
+const checkCompletedTasks = (tasks, date) => {
+  return Object.values(tasks).some((item) => isEqual(item.date, date) && item.checked);
+};
+
+const checkPendingTasks = (tasks, date) => {
+  return Object.values(tasks).some((item) => isEqual(item.date, date) && !item.checked);
+};
+
+const createDayClass = (date, currentDate) => {
+  let dayClass = 'card__day';
+
+  if (isToday(date)) {
+    dayClass += ' card__day_today';
+  }
+
+  if (isEqual(currentDate, date)) {
+    dayClass += ' card__day_current';
+  }
+  return dayClass;
+};
+
+const CalendarCard = ({ date }) => {
+  const { tasks, currentDate, setCurrentDate } = useApp();
   console.log('calendar card');
 
-  const checkFulfilledTasks = useMemo(() => {
-    return Object.values(tasks).some((item) => isEqual(item.date, date) && item.checked);
-  }, [tasks]);
-
-  const checkPendingTasks = useMemo(() => {
-    return Object.values(tasks).some((item) => isEqual(item.date, date) && !item.checked);
-  }, [tasks]);
+  const dayClass = useMemo(() => createDayClass(date, currentDate), [currentDate]);
+  const isCompletedTask = useMemo(() => checkCompletedTasks(tasks, date), [tasks]);
+  const isPendingTask = useMemo(() => checkPendingTasks(tasks, date), [tasks]);
 
   return (
-    <button type="button" className="card btn" onClick={() => onClick(date)}>
-      <div className={createDayClass(date)}>
+    <button type="button" className="card btn" onClick={() => setCurrentDate(date)}>
+      <div className={dayClass}>
         <span className="card__text">{format(date, 'eee')}</span>
-        <span className="card__number">{date.getDate()}</span>
+        <span className="card__number">{getDate(date)}</span>
       </div>
       <div className="card__board">
-        {checkPendingTasks ? <div className="card__pending" /> : null}
-        {checkFulfilledTasks ? <div className="card__fulfilled" /> : null}
+        {isPendingTask ? <div className="card__pending" /> : null}
+        {isCompletedTask ? <div className="card__fulfilled" /> : null}
       </div>
     </button>
   );
-}
+};
 
 CalendarCard.propTypes = {
-  date: PropTypes.object,
-  onClick: PropTypes.func,
-  checkPendingTasks: PropTypes.func,
-  checkFulfilledTasks: PropTypes.func,
-  createDayClass: PropTypes.func,
+  date: PropTypes.number,
 };
 
 export default React.memo(CalendarCard);
